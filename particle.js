a7b9aff37ec756dff30f9215529cd924b2ebcd68
Gayathri-n123 <74848845+Gayathri-n123@users.noreply.github.com>
2021-05-01 21:03:20 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,32 @@
+class Particle{
+
+    constructor(x,y){
+        var options={
+
+            restitution:0.8,
+            friction:0.2,
+        }
+        
+        this.radius=10
+        this.color=color(random(0,255),random(0,255),random(0,255));
+        this.body=Bodies.circle(x,y,this.radius,options)
+
+        World.add(world,this.body);
+    }
+
+
+
+    display(){
+
+       var pos = this.body.position;
+       var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+    }
+}
